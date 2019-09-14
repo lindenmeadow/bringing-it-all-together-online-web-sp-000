70357b42616d96df742f58f074ab17f4331f00c0
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-14 18:36:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,4 +22,12 @@ class Dog
     sql = "DROP TABLE IF EXISTS dogs"
     DB[:conn].execute(sql)
   end
+
+  def self.new_from_db(row)
+    new_dog = self.new
+    new_dog.id = row[0]
+    new_dog.name = row[1]
+    new_dog.breed = row[2]
+    new_dog
+  end
 end
