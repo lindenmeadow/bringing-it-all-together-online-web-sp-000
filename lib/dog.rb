e338ab8275e7893f90511fc0deb0dac0c2835cd7
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-09-14 18:58:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -24,11 +24,10 @@ class Dog
   end
 
   def self.new_from_db(row)
-    new_dog = self.new
-    new_dog.id = row[0]
-    new_dog.name = row[1]
-    new_dog.breed = row[2]
-    new_dog
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(name: name, breed: breed, id: id)
   end
 
   def self.find_by_name(name)
